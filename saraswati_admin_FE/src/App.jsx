463c72ad51fa6021197141f41d800d367d5558a4
Arpit joshi <your-arpitjoshi@gmail.com>
2025-02-10 21:50:02 +0530

    initial template

---
+++ saraswati_admin_FE/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react' ;
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <p className='text-center'>Saraswati admin frontend</p>
+    </>
+  )
+}
+
+export default App
