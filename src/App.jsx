68714fc59e3148a37a6f707b77e1d43e4fa40b57
Manish Chandra Paneru <manishpaneru2010@gmail.com>
2025-02-15 22:17:21 +0530

    Add Leaderboard component and integrate react-router-dom for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Leaderboard from './Leaderboard'; // Import the Leaderboard component
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="bg-black text-white">Saraswati Admin</div>
-    </>
-  )
+    <Router>
+      {/* <div className="bg-black text-white">Saraswati Admin</div> */}
+      {/* <div><Link to="/leaderboard">Leaderboard</Link></div> */}
+      <Routes>
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
